fffd0850e36676c2cceffb630e39843f9313193e
Robert Marchino <rmarchino95@gmail.com>
2024-06-17 23:58:23 -0500

    Agregando ssl al proyecto

--- src/config/config.js
+++ src/config/config.js
@@ -23,6 +23,11 @@ module.exports = {
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     dialect: "postgres",
-    dialectOptions: {},
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
